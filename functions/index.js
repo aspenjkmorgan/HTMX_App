d065a8e83bce9e1e7ef89da68ab0d5bc79fd5619
hotcasper <122695959+hotcasper@users.noreply.github.com>
2024-04-24 09:39:11 -0600

    first commit

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const pug = require('pug');
+
+exports.test = onRequest((request, response) => {
+    let template = pug.compileFile('views/test.pug');
+    let markup = template({
+        name: request.query.name,
+        major: request.query.major,
+        quote: request.query.quote
+    })
+    response.writeHead(200, {'Content-Type' : 'text/html'});
+    response.end(markup)
+});
