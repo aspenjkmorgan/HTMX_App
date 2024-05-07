2d6c81c028ab7249e44a8d034305a7cf577fee89
hotcasper <122695959+hotcasper@users.noreply.github.com>
2024-05-06 21:30:09 -0600

    ninty percent

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,54 @@
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const admin = require('firebase-admin');
 const pug = require('pug');
 
-exports.test = onRequest((request, response) => {
-    let template = pug.compileFile('views/test.pug');
-    let markup = template({
-        name: request.query.name,
-        major: request.query.major,
-        quote: request.query.quote
-    })
-    response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.end(markup)
+// hide firebase api key
+const key = process.env.FB_KEY;
+
+const firebaseConfig = {
+    apiKey: key,
+    authDomain: "example-project4.firebaseapp.com",
+    projectId: "example-project4",
+    storageBucket: "example-project4.appspot.com",
+    messagingSenderId: "199449270796",
+    appId: "1:199449270796:web:9bfeb311a4e0241678ac29",
+    measurementId: "G-2SVV193YVQ"
+}; 
+  
+admin.initializeApp(firebaseConfig);
+const db = admin.firestore();
+
+exports.search = onRequest(async (request, response) => {
+  // what the user searched
+  const searchTerm = request.body;
+
+  // deposite city names from database
+  let namesList = []
+  const snapshot = await db.collection('Cities').get();
+  snapshot.forEach(doc => {
+    // get the name field from each document
+    const name = doc.data().name;
+    // add the name to the list
+    namesList.push(name);
+  });
+
+  // empty array to store matches
+  let matches = [];
+
+  // Loop through the list
+  for (let item of namesList) {
+      // Check if the item contains the searchTerm
+      if (item.includes(searchTerm)) {
+          // If it does, push it to the matches array
+          matches.push(item);
+      }
+  }
+
+  // use pug to generate html table of matches
+  const template = pug.compileFile("views/format_table.pug");
+  const html = template({ matches });
+
+  // return the html table
+  response.send(html);
 });
+
